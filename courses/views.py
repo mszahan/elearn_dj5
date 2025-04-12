57b814433e733efde92074675771eaeb5e81c6c4
mszahan <mszahan1@gmail.com>
2025-04-12 23:12:13 +0600

    created list and deatail page for the course

--- courses/views.py
+++ courses/views.py
@@ -5,8 +5,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.forms.models import modelform_factory
-from .models import Course, Module, Content
+from django.db.models import Count
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 
@@ -154,4 +156,21 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects':subjects, 'subject':subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
     
