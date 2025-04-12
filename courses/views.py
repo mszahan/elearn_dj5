d1fe7320dce529c1388787cea11bead8f2f2ebae
mszahan <mszahan1@gmail.com>
2025-04-12 20:33:06 +0600

    added drag and drop option for ordering the course module

--- courses/views.py
+++ courses/views.py
@@ -8,6 +8,7 @@ from django.views.generic.list import ListView
 from django.forms.models import modelform_factory
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
+from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 
 
 
@@ -140,3 +141,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({'module': module})
 
+
+class ModuleOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+    
