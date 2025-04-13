a22ada83c5ca8e99d3ee07a0d22acd8efc2d9898
mszahan <mszahan1@gmail.com>
2025-04-13 15:09:48 +0600

    created student registration view and template

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #local
-    'courses'
+    'courses',
+    'students',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('course/', include('courses.urls')),
     path('', CourseListView.as_view(), name='course_list'),
+    path('course/', include('courses.urls')),
+    path('students/', include('students.urls')),
 ]
 
 if settings.DEBUG:
